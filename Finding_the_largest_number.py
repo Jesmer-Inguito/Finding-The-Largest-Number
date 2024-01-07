9ac74b76ccd24af88a0b1bdd135798cf84f4b24f
unknown <you@example.com>
2024-01-07 17:24:50 +0800

    pseudocode

---
+++ Finding_the_largest_number.py
@@ -0,0 +1,13 @@
+# Finding the largest out of the three numbers
+
+#psedocode
+
+#Ask user to input three different numbers
+
+#Check if num_1 is the biggest
+
+#Check if num_2 is the biggest
+
+#If neither of the two is the biggest, num_3 is the biggest
+
+#Display the biggest number
