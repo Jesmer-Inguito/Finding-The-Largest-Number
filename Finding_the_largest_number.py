07bad362f52fa6a5554f4d7ea08d81731efa7861
unknown <you@example.com>
2024-01-07 17:32:10 +0800

    user input

--- Finding_the_largest_number.py
+++ Finding_the_largest_number.py
@@ -3,6 +3,9 @@
 #pseudocode
 
 #Ask user to input three different numbers
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+num_3 = int(input("Enter the third number: "))
 
 #Check if num_1 is the biggest
 
