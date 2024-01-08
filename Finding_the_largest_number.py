62f75070a6e0b7c8f696fea679a526de3b1ef596
unknown <you@example.com>
2024-01-08 08:55:36 +0800

    made it into a function

--- Finding_the_largest_number.py
+++ Finding_the_largest_number.py
@@ -7,22 +7,28 @@ colorama.init(autoreset=True)
 #pseudocode
 
 #Ask user to input three different numbers
-num_1 = int(input(Fore.MAGENTA + Style.BRIGHT + "Enter the first number: " + Fore.YELLOW + Style.BRIGHT))
-num_2 = int(input(Fore.MAGENTA + Style.BRIGHT + "Enter the second number: " + Fore.YELLOW + Style.BRIGHT))
-num_3 = int(input(Fore.MAGENTA + Style.BRIGHT + "Enter the third number: " + Fore.YELLOW + Style.BRIGHT))
+def solve_problem():
+    num_1 = int(input(Fore.MAGENTA + Style.BRIGHT + "Enter the first number: " + Fore.YELLOW + Style.BRIGHT))
+    time.sleep(0.5)
+    num_2 = int(input(Fore.MAGENTA + Style.BRIGHT + "Enter the second number: " + Fore.YELLOW + Style.BRIGHT))
+    time.sleep(0.5)
+    num_3 = int(input(Fore.MAGENTA + Style.BRIGHT + "Enter the third number: " + Fore.YELLOW + Style.BRIGHT))
 
 #Check if num_1 is the biggest
-if num_1 >= num_2 and num_1 >= num_3:
-    largest = num_1
+    if num_1 >= num_2 and num_1 >= num_3:
+        largest = num_1
 
 #Check if num_2 is the biggest
-elif num_2 >= num_1 and num_2 >= num_3:
-    largest = num_2
+    elif num_2 >= num_1 and num_2 >= num_3:
+        largest = num_2
 
 #If neither of the two is the biggest, num_3 is the biggest
-else:
-    largest = num_3
+    else:
+        largest = num_3
 
 #Display the biggest number
-largest_colored = f"{Fore.GREEN}{Style.BRIGHT}{largest}"
-print(Fore.MAGENTA + Style.BRIGHT + "The largest number is:",largest_colored)
+    time.sleep(1)
+    largest_colored = f"{Fore.GREEN}{Style.BRIGHT}{largest}"
+    print(Fore.MAGENTA + Style.BRIGHT + "The largest number is:",largest_colored)
+
+solve_problem()
